c58b0c142abe7532b317f018af90367da1af943f
Kingestif <kingestiff@gmail.com>
2025-09-24 22:20:11 +0300

    refactor: make navbar fixed position

--- app/ui/Navbar.tsx
+++ app/ui/Navbar.tsx
@@ -1,14 +1,14 @@
 export default function Navbar() {
   return (
-    <div className="Navbar text-white flex justify-between text-2xl py-3">
-        <div>Trailwatch</div>
-            <div className="flex gap-10">
-            <div>Search</div>
-            <div>Home</div>
-            <div>Movies</div>
-            <div>Series</div>
-            </div>
-        <div className="bg-primary text-lg font-semibold rounded-md px-3 py-1">Get Started</div>
+    <div className="Navbar text-white flex justify-between text-2xl py-3 bg-black fixed w-full left-0 px-50">
+      <div>Trailwatch</div>
+      <div className="flex gap-10">
+        <div>Search</div>
+        <div>Home</div>
+        <div>Movies</div>
+        <div>Series</div>
+      </div>
+      <div className="bg-primary text-lg font-semibold rounded-md px-3 py-1">Get Started</div>
     </div>
   )
 }
