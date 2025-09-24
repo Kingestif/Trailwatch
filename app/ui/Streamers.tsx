6d112ad9738e1d44435603177ad9a87a9a9533c2
Kingestif <kingestiff@gmail.com>
2025-09-24 22:42:18 +0300

    refactor: improve ui

--- app/ui/Streamers.tsx
+++ app/ui/Streamers.tsx
@@ -2,13 +2,13 @@ import { StreamData } from "../lib/Placeholder";
 
 export default function Streamers() {
   return (
-    <div className="flex gap-20 text-white py-5">
+    <div className="flex gap-10 text-white py-5">
         {StreamData.map((item,idx)=>(
-            <div className="flex items-center gap-3"> 
-                <div key={item.icon} className="w-15 h-15">
-                    <img src={item.icon} className="w-full h-full object-cover filter" alt="Streaming apps" />
-                </div>
-                {idx < StreamData.length-1 && <div className="text-xl font-bold">+</div>}
+            <div className="flex items-center gap-5"> 
+              <div key={item.icon} className="w-15 h-15">
+                  <img src={item.icon} className="w-full h-full object-cover filter" alt="Streaming apps" />
+              </div>
+              {idx < StreamData.length-1 && <div className="text-3xl font-semibold">+</div>}
             </div>
         ))}
     </div>
