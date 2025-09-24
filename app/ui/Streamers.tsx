44175e1041c968e5e97bc32e8bb8694183886283
Kingestif <kingestiff@gmail.com>
2025-09-25 00:32:39 +0300

    refactor: ui update

--- app/ui/Streamers.tsx
+++ app/ui/Streamers.tsx
@@ -4,12 +4,12 @@ export default function Streamers() {
   return (
     <div className="flex gap-10 text-white py-5">
         {StreamData.map((item,idx)=>(
-            <div className="flex items-center gap-5"> 
-              <div key={item.icon} className="w-15 h-15">
-                  <img src={item.icon} className="w-full h-full object-cover filter" alt="Streaming apps" />
-              </div>
-              {idx < StreamData.length-1 && <div className="text-3xl font-semibold">+</div>}
+          <div key={item.icon} className="flex items-center gap-5"> 
+            <div className="w-15 h-15">
+                <img src={item.icon} className="w-full h-full object-cover filter" alt="Streaming apps" />
             </div>
+            {idx < StreamData.length-1 && <div className="text-3xl font-semibold">+</div>}
+          </div>
         ))}
     </div>
   )
