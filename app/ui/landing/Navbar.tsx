31ae82f0518333cc7bf198ba7dd2878dc7aa230c
Kingestif <kingestiff@gmail.com>
2025-09-26 01:22:02 +0300

    refactor: adjust color contrast for better ux

--- app/ui/landing/Navbar.tsx
+++ app/ui/landing/Navbar.tsx
@@ -1,7 +1,7 @@
 export default function Navbar() {
   return (
     <div className="Navbar text-white flex justify-between text-2xl py-3 bg-black fixed w-full left-0 px-50 max-sm:text-sm max-sm:px-2 max-sm:static max-sm:bg-transparent">
-      <div className="text-3xl font-semibold max-sm:text-xl">Trailwatch</div>
+      <div className="text-4xl font-semibold max-sm:text-xl text-primary">Trailwatch</div>
       <div className="flex gap-10 max-sm:hidden">
         <div>Search</div>
         <div>Home</div>
