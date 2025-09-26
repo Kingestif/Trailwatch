1796c6273341e4b1547e78911171a18dc858fbdd
Kingestif <kingestiff@gmail.com>
2025-09-27 02:40:52 +0300

    refactor: use img instead of image

--- app/ui/home/MovieGallary.tsx
+++ app/ui/home/MovieGallary.tsx
@@ -1,5 +1,4 @@
 import { MovieGallaryData } from "@/app/lib/Placeholder";
-import Image from "next/image";
 
 export default function MovieGallary() {
   return (
@@ -10,12 +9,12 @@ export default function MovieGallary() {
           <div key={movie.name} className="flex gap-2">
             <div className="w-30 h-40 bg-gray-500"></div>
             <div className="w-50 h-40 flex flex-col justify-between">
-              <div className="text-xl">{movie.name}</div>
+              <div className="text-xl font-bold">{movie.name}</div>
               <div className="line-clamp-3">{movie.description}</div>
               <div className="flex justify-between">
                 <div>{movie.year}</div>
-                <div className="flex">
-                  <Image src="/star.png" width={20} height={20} alt="Rating"/>
+                <div className="flex items-center gap-1">
+                  <img src="/star.png" className="h-5 w-5" alt="Rating"/>
                   <div>{movie.rating}</div>
                 </div>
               </div>
