523637ed87bdee837b89417e870d131071a48a46
Kingestif <kingestiff@gmail.com>
2025-09-27 00:48:24 +0300

    feat: add homepage

---
+++ app/home/page.tsx
@@ -0,0 +1,9 @@
+import TopBanner from "../ui/home/topBanner";
+
+export default function page() {
+  return (
+    <div className="bg-black h-screen text-white px-50">
+      <TopBanner/>
+    </div>
+  )
+}
