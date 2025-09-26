e6a6b75226582609cff768d2b46c8d9898773b6e
Kingestif <kingestiff@gmail.com>
2025-09-27 00:48:15 +0300

    refactor: made navbar blur

--- app/ui/landing/Navbar.tsx
+++ app/ui/landing/Navbar.tsx
@@ -1,6 +1,6 @@
 export default function Navbar() {
   return (
-    <div className="Navbar text-white flex justify-between text-2xl py-3 bg-black fixed w-full left-0 px-50 max-sm:text-sm max-sm:px-2 max-sm:static max-sm:bg-transparent items-center">
+    <div className="Navbar text-white flex justify-between text-2xl py-3 fixed w-full left-0 px-50 max-sm:text-sm max-sm:px-2 max-sm:static max-sm:bg-transparent items-center backdrop-blur-md bg-black/30">
       <div className="text-4xl font-semibold max-sm:text-xl text-primary">Trailwatch</div>
       <div className="flex gap-10 max-sm:hidden">
         <div>Search</div>
