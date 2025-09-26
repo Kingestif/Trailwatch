8fbeb5dd3be0f1403e0a5ebda5fa922dd32377fc
Kingestif <kingestiff@gmail.com>
2025-09-27 02:41:12 +0300

    feat: add movies & series section

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,10 +1,14 @@
 import MovieGallary from "../ui/home/MovieGallary";
+import Movies from "../ui/home/Movies";
+import Series from "../ui/home/Series";
 import TopBanner from "../ui/home/topBanner";
 
 export default function page() {
   return (
     <div className="bg-black text-white px-50 flex flex-col gap-10">
       <TopBanner/>
+      <Movies/>
+      <Series/>
       <MovieGallary/>
     </div>
   )
---
+++ app/ui/home/Movies.tsx
@@ -0,0 +1,27 @@
+import { MovieGallaryData } from "@/app/lib/Placeholder";
+
+export default function Movies() {
+  return (
+    <div className="flex flex-col gap-5">
+      <div className="text-3xl font-semibold">Popular Movies</div>
+      <div className="flex gap-5 overflow-x-auto hide-scrollbar">
+        {MovieGallaryData.map(movie=>(
+          <div key={movie.name} className="flex flex-col gap-2 font-bold">
+            <div className="w-50 h-60 bg-gray-500"></div>
+            {/* <img src={movie.poster} className="w-50 h-60" alt="Movie Poster" /> */}
+            <div className="flex flex-col">
+              <div className="text-xl">{movie.name}</div>
+              <div className="flex justify-between">
+                <div>{movie.year}</div>
+                <div className="flex items-center gap-1">
+                  <img src="/star.png" className="h-5 w-5" alt="Rating"/>
+                  <div>{movie.rating}</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
