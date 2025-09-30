030586516773071b9e213e2112e24a2aef1b680e
Kingestif <kingestiff@gmail.com>
2025-09-30 22:59:16 +0300

    feat: add details section

---
+++ app/details/page.tsx
@@ -0,0 +1,11 @@
+import MovieDetail from "../ui/details/MovieDetail";
+import Recommended from "../ui/details/Recommended";
+
+export default function page() {
+  return (
+    <div className="bg-black text-gray-100 px-50 flex flex-col gap-10">
+        <MovieDetail/>
+        <Recommended/>
+    </div>
+  )
+}
