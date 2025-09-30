56f264039c3bce329cb16138af9918f9b1aca2b7
Kingestif <kingestiff@gmail.com>
2025-10-01 01:57:25 +0300

    fix: add z-index to put navbar at the top

--- app/ui/landing/Navbar.tsx
+++ app/ui/landing/Navbar.tsx
@@ -1,6 +1,6 @@
 export default function Navbar() {
   return (
-    <div className="Navbar text-white flex justify-between text-2xl py-3 fixed w-full left-0 px-50 max-sm:text-sm max-sm:px-2 max-sm:static items-center backdrop-blur-md bg-black">
+    <div className="Navbar z-50 text-white flex justify-between text-2xl py-3 fixed w-full left-0 px-50 max-sm:text-sm max-sm:px-2 max-sm:static items-center backdrop-blur-md bg-black">
       <div className="text-4xl font-semibold max-sm:text-xl text-primary">Trailwatch</div>
       <div className="flex gap-10 max-sm:hidden">
         <div>Search</div>
