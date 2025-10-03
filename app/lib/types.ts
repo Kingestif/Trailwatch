9457f274b25b7bd2c51d93c1f99b6ff8c75fc62a
Kingestif <kingestiff@gmail.com>
2025-10-03 23:56:54 +0300

    feat: add type for trengin movies and series

---
+++ app/lib/types.ts
@@ -0,0 +1,9 @@
+export type TrendingMovies = {
+    id:string
+    title:string,
+    name:string,
+    poster_path:string,
+    vote_average:string,
+    release_date:string,
+    first_air_date:string
+}
