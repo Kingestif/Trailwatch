d831ded17f1679e3c373227258d265413537fac9
Kingestif <kingestiff@gmail.com>
2025-10-03 23:57:12 +0300

    feat: integratied movies page with TMDB api

--- app/ui/home/Movies.tsx
+++ app/ui/home/Movies.tsx
@@ -1,26 +1,50 @@
 import { MovieGallaryData } from "@/app/lib/Placeholder";
+import { TrendingMovies } from "@/app/lib/types";
+import Image from "next/image";
 
-export default function Movies() {
+export default async function Movies() {
+  const res = await fetch("https://api.themoviedb.org/3/trending/all/day?language=en-US", {
+    cache:"no-cache",
+    method:"GET",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${process.env.TMDB_API_KEY}`
+    }
+  });
+
+  if (!res.ok) {
+    return (
+      <div className="flex flex-col gap-5">
+        <div className="text-3xl font-semibold">Trending</div>
+        <div className="text-red-500">Failed to load movies.</div>
+      </div>
+    );
+  }
+
+  const trendingMovies = await res.json()
+
   return (
     <div className="flex flex-col gap-5">
       <div className="text-3xl font-semibold">Trending</div>
       <div className="flex gap-5 overflow-x-auto hide-scrollbar">
-        {MovieGallaryData.map(movie=>(
-          <div key={movie.name} className="flex flex-col gap-2 font-bold">
+        {trendingMovies.results?.map((movie:TrendingMovies)=>(
+          <div key={movie.id} className="flex flex-col gap-2 font-bold">
             <div className="w-50 h-60 rounded-lg bg-gray-500">
-              <img
-                src={movie.poster}
-                alt={movie.name}
+              <Image
+                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                alt={movie.title}
                 className="object-cover w-full h-full"
+                width={200}
+                height={300}
               />
             </div>
             <div className="flex flex-col">
-              <div className="text-xl">{movie.name}</div>
+              <div className="text-xl">{movie.title || movie.name}</div>
               <div className="flex justify-between text-gray-300">
-                <div>{movie.year}</div>
+                <div>{(movie.release_date || movie.first_air_date)?.split("-")[0]}</div>
                 <div className="flex items-center gap-1">
                   <img src="/star.png" className="h-5 w-5" alt="Rating"/>
-                  <div>{movie.rating}</div>
+                  <div>{movie.vote_average}</div>
                 </div>
               </div>
             </div>
