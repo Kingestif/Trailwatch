8cbf92b603dcdddbd55b52ebcd537250c122c7cf
Kingestif <kingestiff@gmail.com>
2025-10-04 17:32:42 +0300

    refactor: improve image position

--- app/ui/details/Recommended.tsx
+++ app/ui/details/Recommended.tsx
@@ -38,7 +38,7 @@ export default function Recommended({id, media_type}:recommendedType) {
                     <div key={movie.id} className="flex flex-col gap-2 font-bold">
                     <div className="w-50 h-60 rounded-lg bg-gray-500">
                         <Image
-                            src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+                            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
                             alt={movie.title || movie.name}
                             className="object-cover w-full h-full rounded-lg"
                             width={200}
