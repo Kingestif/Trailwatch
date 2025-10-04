d2421a3a66a1cc0a631031881f1308c896af822a
Kingestif <kingestiff@gmail.com>
2025-10-04 09:13:53 +0300

    feat: add types for movies

--- app/lib/types.ts
+++ app/lib/types.ts
@@ -1,9 +1,17 @@
-export type TrendingMovies = {
-    id:string
-    title:string,
-    name:string,
-    poster_path:string,
-    vote_average:string,
-    release_date:string,
-    first_air_date:string
+export type TrendingType = {
+    id: string
+    title: string,
+    name: string,
+    poster_path: string,
+    vote_average: string,
+    release_date: string,
+    first_air_date: string
+}
+
+export type MoviesType = {
+    id: string
+    title: string,
+    poster_path: string,
+    vote_average: string,
+    release_date: string,
 }
