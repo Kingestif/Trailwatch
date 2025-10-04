33bfa0f5088f0074e368d36f0d8e2ec47213a255
Kingestif <kingestiff@gmail.com>
2025-10-04 09:32:11 +0300

    refactor: minor code structure

--- app/details/page.tsx
+++ app/details/page.tsx
@@ -6,9 +6,9 @@ import Movies from "../ui/home/Movies";
 export default function page() {
   return (
     <div className="bg-black text-gray-100 px-50 flex flex-col gap-10 pt-15">
-        <MovieDetail/>
-        <Recommended/>
-        <Movies/>
+      <MovieDetail/>
+      <Recommended/>
+      <Movies/>
     </div>
   )
 }
