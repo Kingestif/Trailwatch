91ca924daef5588780f0a9c2a1b690ba48ed2159
Kingestif <kingestiff@gmail.com>
2025-10-04 17:41:22 +0300

    feat: add to details navigation

--- app/ui/details/Recommended.tsx
+++ app/ui/details/Recommended.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { CollectionType, recommendedType } from "@/app/lib/types";
 import Image from "next/image";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 
 export default function Recommended({id, media_type}:recommendedType) {
@@ -35,27 +36,29 @@ export default function Recommended({id, media_type}:recommendedType) {
             <div className="text-3xl font-semibold">Recommended</div>
             <div className="flex gap-5 overflow-x-auto hide-scrollbar">
                 {movie.results?.map((movie:CollectionType)=>(
-                    <div key={movie.id} className="flex flex-col gap-2 font-bold">
-                    <div className="w-50 h-60 rounded-lg bg-gray-500">
-                        <Image
-                            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                            alt={movie.title || movie.name}
-                            className="object-cover w-full h-full rounded-lg"
-                            width={200}
-                            height={300}
-                        />
-                    </div>
-                    <div className="flex flex-col">
-                        <div className="text-xl">{movie.name || movie.title}</div>
-                        <div className="flex justify-between text-gray-300">
-                        <div>{(movie.release_date || movie.first_air_date).split("-")[0]}</div>
-                        <div className="flex items-center gap-1">
-                            <img src="/star.png" className="h-5 w-5" alt="Rating"/>
-                            <div>{movie.vote_average}</div>
-                        </div>
-                        </div>
-                    </div>
-                    </div>
+                    <Link key={movie.id} href={`/details/${movie.id}?type=${media_type}`}>
+                      <div className="flex flex-col gap-2 font-bold">
+                      <div className="w-50 h-60 rounded-lg bg-gray-500">
+                          <Image
+                              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                              alt={movie.title || movie.name}
+                              className="object-cover w-full h-full rounded-lg"
+                              width={200}
+                              height={300}
+                          />
+                      </div>
+                      <div className="flex flex-col">
+                          <div className="text-xl">{movie.name || movie.title}</div>
+                          <div className="flex justify-between text-gray-300">
+                          <div>{(movie.release_date || movie.first_air_date).split("-")[0]}</div>
+                          <div className="flex items-center gap-1">
+                              <img src="/star.png" className="h-5 w-5" alt="Rating"/>
+                              <div>{movie.vote_average}</div>
+                          </div>
+                          </div>
+                      </div>
+                      </div>
+                    </Link>
                 ))}
             </div>
         </div>
