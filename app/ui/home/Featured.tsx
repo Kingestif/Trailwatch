03927f8f46798a236116609d7019dbef2c0b44b6
Kingestif <kingestiff@gmail.com>
2025-10-10 21:23:07 +0300

    feat: add trailer video play option

--- app/ui/home/Featured.tsx
+++ app/ui/home/Featured.tsx
@@ -3,10 +3,14 @@ import { Swiper, SwiperSlide } from "swiper/react"
 import "swiper/css";
 import { Autoplay } from "swiper/modules"   
 import { useEffect, useState } from "react";
-import { MoviesType } from "@/app/lib/types";
+import { MoviesType, videoDetail } from "@/app/lib/types";
 
 export default function Featured() {
-  const [movie, setMovie] = useState<{results:MoviesType[] | null}>({results:null});
+  const [movie, setMovie] = useState<{results:MoviesType[]}>({results:[]});
+  const [openTrailer, setOpenTrailer] = useState(false);
+  const [videoKey, setVideoKey] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [noTrailer, setNoTrailer] = useState(false);
 
   useEffect(()=> {
     async function fetchPopularMovie(){
@@ -25,7 +29,29 @@ export default function Featured() {
     fetchPopularMovie();
   },[])
 
-  if (!movie.results) {
+  async function fetchDetails(movieID:string){
+    setLoading(true)
+    const res = await fetch(`https://api.themoviedb.org/3/movie/${movieID}/videos?language=en-US`, {
+      cache:"no-store",
+      method:"GET",
+      headers:{
+        "Content-Type":"application/json",
+        "Authorization":`Bearer ${process.env.NEXT_PUBLIC_TMDB_API_KEY}`
+      }
+    })
+    
+    const data = await res.json();
+    const trailer:videoDetail = data.results.find((vid:videoDetail)=> vid.site === "YouTube")
+    if(data.results.length > 0) {
+      setVideoKey(trailer.key)
+      setOpenTrailer(true)
+    }else{
+      setNoTrailer(true)
+    }
+    setLoading(false);
+  }
+
+  if (movie.results.length < 1) {
     return (
       <div className="relative w-full h-100 overflow-hidden rounded-lg bg-gray-900">
         <div className="absolute inset-0 bg-gradient-to-r from-gray-800 via-gray-700 to-gray-800 animate-[pulse_1.5s_ease-in-out_infinite]" />
@@ -39,9 +65,8 @@ export default function Featured() {
     );
   }
 
-
   return (
-    <div className="overflow-hidden">
+    <div className="overflow-hidden relative">
       <Swiper
         slidesPerView={1}
         loop={true}
@@ -77,7 +102,9 @@ export default function Featured() {
                 </div>
                 {/* Buttons */}
                 <div className="flex gap-3">
-                  <button className="flex items-center bg-white text-black px-5 py-2 rounded-full gap-2 font-bold">
+                  <button className="flex items-center bg-white text-black px-5 py-2 rounded-full gap-2 font-bold"
+                    onClick={()=>{fetchDetails(item.id)}}
+                  >
                     <svg xmlns="http://www.w3.org/2000/svg" width={30} viewBox="0 0 640 640"><path d="M187.2 100.9C174.8 94.1 159.8 94.4 147.6 101.6C135.4 108.8 128 121.9 128 136L128 504C128 518.1 135.5 531.2 147.6 538.4C159.7 545.6 174.8 545.9 187.2 539.1L523.2 355.1C536 348.1 544 334.6 544 320C544 305.4 536 291.9 523.2 284.9L187.2 100.9z"/></svg>
                     Play
                   </button>
@@ -91,6 +118,58 @@ export default function Featured() {
           </SwiperSlide>
         ))}
       </Swiper>
+
+      {
+        loading && 
+        <div>
+          <div className="fixed inset-0 bg-black/70 z-40"></div>
+          <div className="w-100 h-50 text-black fixed left-[50%] top-[50%] translate-[-50%] z-50">
+            <svg className="animate-spin h-30 w-30  text-white" viewBox="0 0 24 24">
+              <circle className="opacity-25" cx="12" cy="12" r="10" stroke="white" strokeWidth="4" fill="none"/>
+              <path className="opacity-75" fill="white" d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"/>
+            </svg>
+          </div>
+        </div>
+      }
+
+      {
+        openTrailer &&
+        <>
+          <div className="fixed inset-0 bg-black/70 z-40"></div>
+
+          <div className="w-300 h-200 text-black fixed left-[50%] top-[50%] translate-[-50%] z-50">
+            <button
+              className="absolute top-2 right-2 bg-black/70 text-white rounded-full px-3 py-1 z-50"
+              onClick={() => setOpenTrailer(false)}
+            >
+              <div className="text-xl font-bold">✕</div>
+            </button>
+            <iframe className="w-full h-full" src={`https://www.youtube.com/embed/${videoKey}?autoplay=1&loop=1&playlist=${videoKey}`} allow="autoplay; encrypted-media" allowFullScreen ></iframe>
+          </div>
+        </>
+      }
+
+      {
+        noTrailer && 
+        <div>
+          <div className="fixed inset-0 bg-black/70 z-40"></div>
+          <div className="w-100 h-50 text-black bg-black fixed left-[50%] top-[50%] translate-[-50%] z-50 gap-5 flex flex-col justify-center items-center">
+            <svg width="48" height="48" viewBox="0 0 24 24" className="mx-auto mb-2 text-red-500" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <circle cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="2"/>
+              <line x1="12" y1="8" x2="12" y2="13" stroke="currentColor" strokeWidth="2" strokeLinecap="round"/>
+              <circle cx="12" cy="16" r="1" fill="currentColor"/>
+            </svg>
+            <div className="text-red-500">Sorry, we couldn't find a trailer for this movie.</div>
+            <button
+              className="absolute top-2 right-2 bg-black/70 text-white rounded-full px-3 py-1 z-50"
+              onClick={() => setNoTrailer(false)}
+            >
+              <div className="text-xl font-bold">✕</div>
+            </button>
+          </div>
+        </div>
+      }
+
     </div>
   )
 }
