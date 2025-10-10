070178ad648424fcfaf8e9544a3897175d942ae1
Kingestif <kingestiff@gmail.com>
2025-10-10 22:00:47 +0300

    refactor: adjust code structure

--- app/ui/details/Recommended.tsx
+++ app/ui/details/Recommended.tsx
@@ -5,64 +5,71 @@ import Link from "next/link";
 import { useEffect, useState } from "react";
 
 export default function Recommended({id, media_type}:recommendedType) {
-    const [movie, setMovie] = useState<{results:CollectionType[] | null}>({ results: null });
+  const [movie, setMovie] = useState<{results:CollectionType[]}>({ results: [] });
+  const [loading, setLoading] = useState(false);
 
-    useEffect(()=>{
-        async function fetchData(){
-          let url = ""
-          if(media_type === "movie"){
-            url = `https://api.themoviedb.org/3/movie/${id}/recommendations?language=en-US&page=1`
-          }else{
-            url = `https://api.themoviedb.org/3/tv/${id}/recommendations?language=en-US&page=1`
-          }
-    
-          const res = await fetch(url,{
-            cache:"no-store",
-            method:"GET",
-            headers: {
-              "Content-Type":"application/json",
-              "Authorization": `Bearer ${process.env.NEXT_PUBLIC_TMDB_API_KEY}`
-            }
-          })
-          const data = await res.json();
-          setMovie(data);
+  useEffect(()=>{
+    async function fetchData(){
+      setLoading(true);
+      let url = ""
+      if(media_type === "movie"){
+        url = `https://api.themoviedb.org/3/movie/${id}/recommendations?language=en-US&page=1`
+      }else{
+        url = `https://api.themoviedb.org/3/tv/${id}/recommendations?language=en-US&page=1`
+      }
+
+      const res = await fetch(url,{
+        cache:"no-store",
+        method:"GET",
+        headers: {
+          "Content-Type":"application/json",
+          "Authorization": `Bearer ${process.env.NEXT_PUBLIC_TMDB_API_KEY}`
         }
-    
-        fetchData();
-    },[id, media_type]);
+      })
+      const data = await res.json();
+      setMovie(data);
+      setLoading(false);
+    }
+  
+    fetchData();
+  },[id, media_type]);
 
-    return (
-        <div className="flex flex-col gap-5 py-5">
-            <div className="text-3xl font-semibold">Recommended</div>
-            <div className="flex gap-5 overflow-x-auto hide-scrollbar">
-                {movie.results?.map((movie:CollectionType)=>(
-                    <Link key={movie.id} href={`/details/${movie.id}?type=${media_type}`}>
-                      <div className="flex flex-col gap-2 font-bold focus:text-yellow-400" tabIndex={0}>
-                        <div className="w-50 h-60 rounded-lg bg-gray-500 relative">
-                          <Image
-                            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                            alt={movie.title || movie.name}
-                            className="object-cover w-full h-full rounded-lg"
-                            width={200}
-                            height={300}
-                          />
-                          <div className="hover:bg-black/30 absolute w-full h-full top-0 transition-all border-transparent hover:border hover:border-white rounded-lg duration-300"></div>
-                          
-                        </div>
-                        <div className="flex flex-col">
-                            <div className="text-xl">{movie.name || movie.title}</div>
-                            <div className="flex justify-between text-gray-300">
-                            <div>{(movie.release_date || movie.first_air_date || "Unknown").split("-")[0]}</div>
-                            <div className="flex items-center gap-1">
-                                <img src="/star.png" className="h-5 w-5" alt="Rating"/>
-                                <div>{movie.vote_average}</div>
-                            </div>
-                            </div>
-                        </div>
-                      </div>
-                    </Link>
-                ))}
+  if (movie.results.length < 1) {
+    return null;
+  }
+
+  return (
+    <div className="flex flex-col gap-5 py-5">
+      <div className="text-3xl font-semibold">Recommended</div>
+      <div className="flex gap-5 overflow-x-auto hide-scrollbar">
+        {movie.results?.map((movie:CollectionType)=>(
+          <Link key={movie.id} href={`/details/${movie.id}?type=${media_type}`}>
+            <div className="flex flex-col gap-2 font-bold focus:text-yellow-400" tabIndex={0}>
+              <div className="w-50 h-60 rounded-lg bg-gray-500 relative">
+                <Image
+                  src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                  alt={movie.title || movie.name}
+                  className="object-cover w-full h-full rounded-lg"
+                  width={200}
+                  height={300}
+                />
+                <div className="hover:bg-black/30 absolute w-full h-full top-0 transition-all border-transparent hover:border hover:border-white rounded-lg duration-300"></div>
+                
+              </div>
+              <div className="flex flex-col">
+                  <div className="text-xl">{movie.name || movie.title}</div>
+                  <div className="flex justify-between text-gray-300">
+                  <div>{(movie.release_date || movie.first_air_date || "Unknown").split("-")[0]}</div>
+                  <div className="flex items-center gap-1">
+                      <img src="/star.png" className="h-5 w-5" alt="Rating"/>
+                      <div>{movie.vote_average}</div>
+                  </div>
+                  </div>
+              </div>
             </div>
-        </div>
-    )
+          </Link>
+        ))}
+      </div>
+    </div>
+  )
 }
