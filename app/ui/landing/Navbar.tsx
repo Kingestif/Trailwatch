68134698dfb4170d0466a888adabb983814017a7
Kingestif <kingestiff@gmail.com>
2025-10-11 10:56:24 +0300

    refactor: add profile page on navbar

--- app/ui/landing/Navbar.tsx
+++ app/ui/landing/Navbar.tsx
@@ -145,9 +145,20 @@ export default function Navbar() {
           </div>
         }
       </div>
-      <Link href={"/home"}>
-        <div className="bg-primary text-lg font-semibold rounded-md px-3 py-1 max-sm:text-sm max-sm:px-2 max-sm:py-1 flex items-center hover:bg-red-700 duration-200">Get Started</div>
-      </Link>
+
+      {
+        currentPath === "/"? 
+        <Link href={"/home"}>
+          <div className="bg-primary text-lg font-semibold rounded-md px-3 py-1 max-sm:text-sm max-sm:px-2 max-sm:py-1 flex items-center hover:bg-red-700 duration-200">Get Started</div>
+        </Link>
+        :
+        <div className=" rounded-full bg-gray-500 p-1 max-sm:px-2 max-sm:py-1 flex items-center hover:bg-gray-600 duration-200 cursor-pointer">
+          <svg width="40" height="40" viewBox="0 0 24 24" className="text-white" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="12" cy="8" r="4" stroke="currentColor" strokeWidth="2"/>
+            <path d="M4 20c0-2.21 3.58-4 8-4s8 1.79 8 4" stroke="currentColor" strokeWidth="2" strokeLinecap="round"/>
+          </svg>
+        </div>
+      }
     </div>
   )
 }
